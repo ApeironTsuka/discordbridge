8a4e4028e3547b3dfc0e0c14007d2196311d1848
Apeiron <apeiron@none>
2019-02-02 01:55:40 -0600

    Fix [] not converting to {}
    Add something to todo list...

--- discord/modules/main/bridge.js
+++ discord/modules/main/bridge.js
@@ -185,8 +185,9 @@ function handleMsgs(m) {
   if (type === undefined) { return; }
   switch (m.channel.name) { case 'party': case 'raid': if (!this._proxy.avail[m.channel.name]) { m.channel.send(`You are not in a ${m.channel.name}`); return; } }
   if (m.channel.__muted) { m.channel.send('Shh, you\'re still muted'); return; }
+  let msg = m.cleanContent.replace(/\[/g, '{').replace(/\]/g, '}');
   if (!sendMsg(this._proxy.client, type, m.channel.name, m.cleanContent)) {
-    let msg = m.cleanContent.substr(0,300);
+    msg = msg.substr(0,300);
     m.channel.send(`Max message length (300) exceeded. Only sent: ${msg}`);  
   }
   this._proxy.lastSource = m.channel;
