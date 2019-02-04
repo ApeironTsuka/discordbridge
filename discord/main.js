0479a2ec502191a0ecfd66d19d6132ae1f1cb328
Apeiron <apeiron@none>
2019-02-03 20:09:29 -0600

    Derp

--- discord/main.js
+++ discord/main.js
@@ -1,7 +1,7 @@
 'use strict';
 const fs = require('fs');
 console._log = console.log;
-console.log = function (...args) { console._log('[discord]', ...args); fs.appendFileSync('log', `${...args}\n`); };
+console.log = function (...args) { console._log('[discord]', ...args); fs.appendFileSync('log', `${args.join(' ')}\n`); };
 fs.writeFileSync('log', '');
 console.log('Loading..');
 const Discord = require('discord.js'), util = require('util'),
